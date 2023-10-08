02a7bf9974beb313714ae97f5a9e435f757f5497
Julius Hoffmann <36714809+Hoffmann77@users.noreply.github.com>
2023-10-08 18:39:52 +0200

    test: add new test

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -126,44 +126,44 @@ async def test_with_3_9_36_firmware():
     }
 
 
-# @pytest.mark.asyncio
-# @respx.mock
-# async def test_with_7_6_175_firmware():
-#     """Test with 7.6.175 firmware."""
-#     # Config --->
-#     fixture_name = "7.6.175_EnvoySMetered"
-#     gateway_class = "EnvoySMetered"
+@pytest.mark.asyncio
+@respx.mock
+async def test_with_7_6_175_firmware():
+    """Test with 7.6.175 firmware."""
+    # Config --->
+    fixture_name = "7.6.175_EnvoySMetered"
+    gateway_class = "EnvoySMetered"
 
-#     gateway = await get_gateway(fixture_name)
-#     print(gateway.required_endpoints)
-#     print(gateway.data)
+    gateway = await get_gateway(fixture_name)
+    print(gateway.required_endpoints)
+    print(gateway.data)
 
-#     # gateway class
-#     assert gateway.__class__.__name__ == gateway_class
+    # gateway class
+    assert gateway.__class__.__name__ == gateway_class
 
-#     # meter configuration
-#     assert gateway.production_meter == 704643328
-#     assert gateway.net_consumption_meter == 704643584
+    # meter configuration
+    assert gateway.production_meter == 704643328
+    assert gateway.net_consumption_meter == 704643584
 
-#     # production data
-#     assert gateway.production == 488.925
-#     assert gateway.daily_production == 4425.303
-#     assert gateway.seven_days_production == 111093.303
-#     assert gateway.lifetime_production == 3183793.885
-#     assert gateway.consumption is not None
-#     assert gateway.daily_consumption == 19903.621
-#     assert gateway.seven_days_consumption == 4.621
-#     assert gateway.lifetime_consumption is not None
+    # production data
+    assert gateway.production == 488.925
+    assert gateway.daily_production == 4425.303
+    assert gateway.seven_days_production == 111093.303
+    assert gateway.lifetime_production == 3183793.885
+    assert gateway.consumption is not None
+    assert gateway.daily_consumption == 19903.621
+    assert gateway.seven_days_consumption == 4.621
+    assert gateway.lifetime_consumption is not None
 
-#     # battery data
-#     assert gateway.encharge_inventory is None
-#     assert gateway.encharge_power is None
+    # battery data
+    assert gateway.encharge_inventory is None
+    assert gateway.encharge_power is None
 
-#     # inverters
-#     assert gateway.inverters_production["482243031579"] == {
-#         "serialNumber": "482243031579",
-#         "lastReportDate": 1693744825,
-#         "devType": 1,
-#         "lastReportWatts": 135,
-#         "maxReportWatts": 365
-#     }
+    # inverters
+    assert gateway.inverters_production["482243031579"] == {
+        "serialNumber": "482243031579",
+        "lastReportDate": 1693744825,
+        "devType": 1,
+        "lastReportWatts": 135,
+        "maxReportWatts": 365
+    }
